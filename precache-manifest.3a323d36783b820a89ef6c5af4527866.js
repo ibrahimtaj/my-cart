a46d6ef175f0aee40aa0d466e28ab23546724946
ibrahimtaj <taj_it@yahoo.com>
2020-05-02 13:15:45 +0530

    Updates

---
+++ precache-manifest.3a323d36783b820a89ef6c5af4527866.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10aaf4e214a297d14d6b4b60ed9a4ac",
+    "url": "/my-cart/index.html"
+  },
+  {
+    "revision": "72a5ab4b35b531f3bb94",
+    "url": "/my-cart/static/css/main.36f87d4d.chunk.css"
+  },
+  {
+    "revision": "9e1f9eba71fcc9409680",
+    "url": "/my-cart/static/js/2.b3ed77bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-cart/static/js/2.b3ed77bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a5ab4b35b531f3bb94",
+    "url": "/my-cart/static/js/main.2fd3a7b3.chunk.js"
+  },
+  {
+    "revision": "aae8d306b92ccd5a3967",
+    "url": "/my-cart/static/js/runtime-main.8abaa8f0.js"
+  },
+  {
+    "revision": "02ed288f8e38d26052e622bf7174559f",
+    "url": "/my-cart/static/media/laptop.02ed288f.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/my-cart/precache-manifest.3a323d36783b820a89ef6c5af4527866.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-cart/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
